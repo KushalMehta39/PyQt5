ce215083e6ecda19f5e3817c8380e76e37ee8215
KushalMehta39 <kushalmehta0309@gmail.com>
2021-12-13 19:06:54 +0530

    Code added in Day 1

---
+++ tutorial_1.py
@@ -0,0 +1,9 @@
+from PyQt5 import Qtwidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
