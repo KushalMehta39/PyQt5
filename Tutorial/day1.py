13675150ece1fa7b3a78da0ecbc4ace0177776a5
KushalMehta39 <89148778+KushalMehta39@users.noreply.github.com>
2021-12-16 22:44:59 +0530

    Update day1.py

--- Tutorial/day1.py
+++ Tutorial/day1.py
@@ -1,1 +1,12 @@
+from PyQt5 import Qtwidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+© 2021 GitHub, Inc.
+Terms
+Privacy
